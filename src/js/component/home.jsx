758f06131f843653ac9ce73955671a010e9e452a
Juan Guerrero <juanvicente0104@gmail.com>
2023-10-18 03:26:21 +0000

    project delivery

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	//Define the states
+	const [task, setTask] = useState("");
+	const [taskList, setTaskList] = useState([]);
+
+
+	function handleTask(event) {
+		setTask(event.target.value)
+	}
+
+	function handleTaskList(event) {
+		if (event.key == "Enter") {
+			if (task.trim() !== "") {
+				setTaskList([...taskList, task.trim()]);
+				setTask("");
+			}
+		}
+	}
+
+	function handleDelete(index) {
+		let newtaskList = taskList.filter((item, indexFilter) => index !== indexFilter)
+		setTaskList(newtaskList);
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+		<div className="container d-flex flex-column justify-content-center align-items-center">
+			<h1 className="text-center mt-5">To do list </h1>
+			<input onChange={handleTask} value={task} onKeyDown={handleTaskList} placeholder="Add task" />
+
+			<ul>
+
+				{
+					taskList.map((item, index) => {
+						return (<li key={index} className="border-bottom d-flex justify-content-between">
+							<span> {item} <i onClick={() => handleDelete(index)} class="fas fa-trash"></i> </span>
+
+						</li>)
+					}
+
+					)
+				}
+
+
+			</ul>
+
 			<p>
-				<img src={rigoImage} />
+				{taskList.length == 0 ? 'There are no tasks' : taskList.length == 1 ? 'There is one task' :
+					`There are ${taskList.length} tasks`}
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+
+
+
 		</div>
 	);
 };
